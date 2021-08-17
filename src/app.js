f1b9d26554c47448e49ccecef07f77cdc14049da
tommy <tommysolano18@hotmail.com>
2021-08-17 03:01:06 -0500

    connected to mongo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-    console.log("listen on the port 3000")
+
+//connecting to db
+mongoose.connect("mongodb://localhost/crud-mongo", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+    .then(db => console.log("bd connected"))
+    .catch(err => console.error(err))
+
+//importing routes
+const indexRoutes = require("./routes/index")
+
+//settings
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname + '/views'))
+app.set("view engine", "ejs")
+
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}))
+
+//routes
+app.use("/", indexRoutes);
+
+//starting the server
+app.listen(app.get("port"), () => {
+    console.log(`listen on the port ${app.get("port")}`)
 })
+

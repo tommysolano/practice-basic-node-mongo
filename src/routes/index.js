f13cb94e11349538c248c2b1243c8e25cc0ff3a8
tommy <tommysolano18@hotmail.com>
2021-08-19 03:12:54 -0500

    adding route update

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,42 @@ const Task = require("../models/taks")
 
 router.get("/", async (req, res) => {
     const tasks = await Task.find()
-    res.render ("index", { tasks: tasks})
+    res.render ("index", { 
+        tasks: tasks
+    })
 })
 
 router.post("/add", async (req, res) => {
     const task = new Task(req.body)
     await task.save()
-    res.send("received")
+    res.redirect("/")
+})
+
+router.get("/turn/:id", async (req, res) => {
+    const { id } = req.params
+    const task = await Task.findById(id)
+    task.status = !task.status
+    await task.save()
+})
+
+router.get("/edit/:id", async (req, res) => {
+    const { id } = req.params
+    const task = await Task.findById(id)
+    res.render("edit", {
+        task
+    })
+})
+
+router.post("/edit/:id", async (req, res) => {
+    const { id } = req.params
+    await Task.update({_id: id}, req.body)
+    res.redirect("/")
+})
+
+router.get("/delete/:id", async (req, res) => {
+    const { id } = req.params
+    await Task.remove({ _id: id })
+    res.redirect("/")
 })
 
 module.exports = router
